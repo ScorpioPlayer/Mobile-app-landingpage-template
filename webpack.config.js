9b758a8e820facd6c878f9bc47557ca83f29d505
bosxixi <bosxixi@outlook.com>
2024-09-07 10:36:22 +1200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
-module.exports = function getWebpackConfig(env) {
-  // eslint-disable-next-line global-require, import/no-dynamic-require
-  return require(`./config/webpack.${env.config}.js`);
-};
+module.exports = function getWebpackConfig(env) {  
+  // eslint-disable-next-line global-require, import/no-dynamic-require  
+  const config = require(`./config/webpack.${env.config}.js`);  
+  
+  // Ensure the devServer configuration exists  
+  if (!config.devServer) {  
+    config.devServer = {};  
+  }  
+  
+  // Set the host to '0.0.0.0'  
+  config.devServer.host = '0.0.0.0';  
+  
+  return config;  
+};  
